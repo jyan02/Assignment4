f0b5d2e3dbd9370975eb6a7f4d81f48925a65e58
jyan02 <michael.jason.yan@gmail.com>
2017-03-10 11:27:32 -0800

    added more stuff

--- src/edu/cpp/cs/cs141/prog_assgmnt_4/Appointment.java
+++ src/edu/cpp/cs/cs141/prog_assgmnt_4/Appointment.java
@@ -1,5 +1,15 @@
 package edu.cpp.cs.cs141.prog_assgmnt_4;
 
 public class Appointment {
+	
+	private String ownerName;
+	private int apptDate;
+	private int apptTime;
+	
+	public Appointment(String name, int date, int time ){
+		this.ownerName = name;
+		this.apptDate = date;
+		this.apptTime = time;
+	}
 
 }
--- src/edu/cpp/cs/cs141/prog_assgmnt_4/Main.java
+++ src/edu/cpp/cs/cs141/prog_assgmnt_4/Main.java
@@ -1,7 +1,15 @@
 package edu.cpp.cs.cs141.prog_assgmnt_4;
 
+/**This class is the Main class, which is in charge of the running of the game.
+ * @author J
+ *
+ */
 public class Main {
 	
+	/** This method creates an instance of the User Interface and Engine and passes
+	 * the User Interface into the Engine constructor and runs the game. 
+	 * @param args
+	 */
 	public static void main(String[] args){
 		UI ui = new UI();
 		Engine engine = new Engine(ui);
